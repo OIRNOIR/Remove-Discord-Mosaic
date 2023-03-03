4235ea9124e19f0fb17fb9a68f004ef9e4743a0d
OIRNOIR <oirnoir@outlook.com>
2023-03-02 22:31:43 -0800

    First draft

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,124 @@
-import { common, Injector, settings, webpack } from "replugged";
-import { AnyFunction } from "replugged/dist/types";
 import "./style.css";
 
-const inject = new Injector();
-const { React } = common;
+let observer : MutationObserver, observer2 : MutationObserver, observer3 : MutationObserver;
+
+function fixElement(element : Element) {
+	for (const img of element.querySelectorAll('div[class*="mediaAttachmentsContainer-"] div[class*="imageContainer-"] img')) {
+		if (img.getAttribute("rdm-old-params") != null) continue;
+		const src = img.getAttribute("src");
+		if (src == null) continue;
+		let splitSrc = src.split("?");
+		const newSrc = splitSrc.shift() + "?size=1024";
+		if (newSrc == null) continue;
+		img.setAttribute("src", newSrc);
+		img.setAttribute("rdm-old-params", splitSrc.join("?"));
+		console.log("Fixed element", img);
+	}
+}
+
+function unfixAll() {
+	for (const img of document.querySelectorAll('img[rdm-old-params]')) {
+		const src = img.getAttribute("src");
+		if (src == null) continue;
+		let splitSrc = src.split("?");
+		const newSrc = splitSrc.shift() + `?${img.getAttribute("rdm-old-params")}`;
+		if (newSrc == null) continue;
+		img.setAttribute("src", newSrc);
+		img.removeAttribute("rdm-old-params");
+		console.log("Unfixed element", img);
+	}
+}
 
 export async function start(): Promise<void> {
-  const observer = new MutationObserver((mutationsList, observer) => {
+  observer = new MutationObserver((mutationsList, observer) => {
 		[...mutationsList].forEach(mutation => {
-			for (const element of Array.from(mutation.addedNodes).filter(n => n.nodeName == "STYLE").length > 0) {
-
+			console.log(mutation);
+			let debugIterations = 0;
+			for (const child of document.querySelectorAll('ol[data-list-id="chat-messages"]')[0].children) {
+				debugIterations++;
+				console.log("debug: " + debugIterations);
+				fixElement(child);
 			}
 		});
 	});
 
-	new MutationObserver((m) => [...m].forEach((mutation) => {
-		if (mutation.target.nodeName == "STYLE" || Array.from(mutation.addedNodes).filter(n => n.nodeName == "STYLE").length > 0 || Array.from(mutation.removedNodes).filter(n => n.nodeName == "STYLE").length > 0) {
-
+	function reconnect() {
+		try {
+			observer.observe(document.querySelectorAll('ol[data-list-id="chat-messages"]')[0], {
+				attributes: true,
+				childList: true,
+				subtree: true
+			});
+			console.log("OBSERVING1");
+			for (const child of document.querySelectorAll('ol[data-list-id="chat-messages"]')[0].children) {
+				fixElement(child);
+			}
+		} catch (error) {
+			console.error(error);
+			setTimeout(() => {
+				reconnect();
+			}, 1000);
 		}
-	})).observe(document.head, {
-		attributes: true,
-		childList: true,
-		subtree: true
-	});
+	}
+
+	function reconnect2() {
+		try {
+			observer2 = new MutationObserver((mutations) => {
+				for (const m of mutations) {
+					for (const n of m.removedNodes) {
+						if (n instanceof Element && Array.from(n.classList).find(c => c.startsWith("chatContent-")) && n.nodeName == "MAIN") {
+							reconnect();
+						}
+					}
+				}
+			})
+			observer2.observe(document.querySelectorAll('div[class*="chat-"] > div[class*="content-"]')[0], {
+				attributes: true,
+				childList: true,
+				subtree: true
+			});
+			console.log("OBSERVING2");
+			reconnect();
+		} catch (error) {
+			console.error(error);
+			setTimeout(() => {
+				reconnect2();
+			}, 1000);
+		}
+	}
+
+	function reconnect3() {
+		try {
+			observer3 = new MutationObserver((mutations) => {
+				for (const m of mutations) {
+					for (const n of m.removedNodes) {
+						if (n instanceof Element && Array.from(n.classList).find(c => c.startsWith("chat-")) && n.nodeName == "DIV") {
+							reconnect2();
+						}
+					}
+				}
+			})
+			observer3.observe(document.querySelectorAll('div[class*="base-"] > div[class*="content-"]')[0], {
+				attributes: true,
+				childList: true,
+				subtree: true
+			});
+			console.log("OBSERVING3");
+			reconnect2();
+		} catch (error) {
+			console.error(error);
+			setTimeout(() => {
+				reconnect3();
+			}, 1000);
+		}
+	}
+
+	reconnect3();
 }
 
 export function stop(): void {
-  
+  observer.disconnect();
+  observer2.disconnect();
+  observer3.disconnect();
+	unfixAll();
 }
