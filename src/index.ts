ce48c6f43bf7743b0c506b49aa9e2975d4b45e91
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2023-10-05 20:15:43 -0700

    Partial fix (Videos in mosaics still broken)

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,75 @@
 import replugged from "replugged";
-
-// Some required code for the old observer method
-/*import "./style.css";
+import "./style.css";
 
 let observer : MutationObserver, observer2 : MutationObserver, observer3 : MutationObserver;
+
+let disabled = false;
+
+function onVideoClick(e: MouseEvent) {
+	if (!disabled) {
+		e.stopPropagation();
+		/*const video = e.target as HTMLVideoElement;
+		console.log("Play State Toggle - Video");
+		if (video.paused) {
+			video.play();
+		} else {
+			video.pause();
+		}*/
+	}
+}
+
+function onCoverClick(e: MouseEvent) {
+	if (!disabled) {
+		e.stopPropagation();
+		const target = (e.target as Element);
+		console.log((e.target as Element).closest('[class*="wrapperMediaMosaic-"]'));
+		const video = (e.target as Element).closest('[class*="wrapperMediaMosaic-"]')?.querySelector("video");
+		if (video == null) return console.log("Video returned null!");
+		console.log("Play State Toggle - Cover");
+		if (video.paused) {
+			video.play();
+		} else {
+			video.pause();
+		}
+		(target as HTMLDivElement).removeEventListener("click", onCoverClick);
+		const cover = (target as HTMLDivElement).closest('[class*="cover-"]') as HTMLDivElement;
+		cover.style.display = "none";
+	}
+}
 
 function fixElement(element : Element) {
 	for (const img of element.querySelectorAll('div[class*="mediaAttachmentsContainer-"] div[class*="imageContainer-"] img')) {
 		if (img.getAttribute("rdm-old-params") != null) continue;
 		const src = img.getAttribute("src");
 		if (src == null) continue;
-		let splitSrc = src.split("?");
-		const newSrc = splitSrc.shift();
+		const srcURL = new URL(src);
+		srcURL.searchParams.delete("width");
+		srcURL.searchParams.delete("height");
+		const newSrc = srcURL.toString();
 		if (newSrc == null) continue;
+		img.setAttribute("rdm-old-params", srcURL.searchParams.toString());
 		img.setAttribute("src", newSrc);
-		img.setAttribute("rdm-old-params", splitSrc.join("?"));
+	}
+	for (const video of element.querySelectorAll('div[class*="mediaAttachmentsContainer-"] video') as NodeListOf<HTMLVideoElement>) {
+		if (video.getAttribute("rdm-old-params") != null) continue;
+		const poster = video.getAttribute("poster");
+		if (poster == null) continue;
+		const posterURL = new URL(poster);
+		posterURL.searchParams.delete("width");
+		posterURL.searchParams.delete("height");
+		const newPoster = posterURL.toString();
+		if (newPoster == null) continue;
+		video.setAttribute("rdm-old-params", posterURL.searchParams.toString());
+		video.setAttribute("poster", newPoster);
+		video.setAttribute("rdm-old-height", video.getAttribute("height") ?? "");
+		video.setAttribute("rdm-old-width", video.getAttribute("width") ?? "");
+		video.removeAttribute("height");
+		video.removeAttribute("width");
+		/*video.addEventListener("click", onVideoClick);
+		const cover = video.parentElement?.querySelector('video + [class*="cover-"]');
+		if (cover) {
+			(cover as HTMLDivElement).addEventListener("click", onCoverClick);
+		}*/
 	}
 }
 
@@ -29,36 +84,25 @@ function unfixAll() {
 		img.removeAttribute("rdm-old-params");
 		console.log("Unfixed element", img);
 	}
-}
-*/
-
-let disabled = false;
-
-type MediaMosaicExperiment = {Z: MediaMosaicExperimentZ};
-type GetCurrentConfigFunction = () => MediaMosaicExperimentCurrentConfig;
-type MediaMosaicExperimentCurrentConfig = {enabled: boolean};
-
-interface MediaMosaicExperimentZ {
-	getCurrentConfig: GetCurrentConfigFunction;
+	for (const video of document.querySelectorAll('video[rdm-old-params]') as NodeListOf<HTMLVideoElement>) {
+		const poster = video.getAttribute("poster");
+		if (poster == null) continue;
+		let splitPoster = poster.split("?");
+		const newPoster = splitPoster.shift() + `?${video.getAttribute("rdm-old-params")}`;
+		if (newPoster == null) continue;
+		video.setAttribute("poster", newPoster);
+		// video.removeEventListener("click", onVideoClick);
+		video.removeAttribute("rdm-old-params");
+		console.log("Unfixed element", video);
+	}
 }
 
 export async function start(): Promise<void> {
 	disabled = false;
 	let start = Date.now();
 
-	// JS interval method (Old)
-	/*const interval : NodeJS.Timer = setInterval(async () => {
-		if (disabled) return clearInterval(interval);
-		// Credit to Alyxia#4650 for the basic method
-		const mediaMosaicExperiment = await replugged.webpack.waitForModule(replugged.webpack.filters.bySource(/media_mosaic/)) as MediaMosaicExperiment;
-		if (mediaMosaicExperiment.Z.getCurrentConfig().enabled == false && Date.now() - start > 10000) {
-			clearInterval(interval);
-		}
-		mediaMosaicExperiment.Z.getCurrentConfig().enabled = false;
-	}, 1000);*/
-
-	// JS observer method (Old)
-  /*observer = new MutationObserver((mutationsList, observer) => {
+	// JS observer method
+  observer = new MutationObserver((mutationsList, observer) => {
 		[...mutationsList].forEach(mutation => {
 			for (const child of document.querySelectorAll('ol[data-list-id="chat-messages"]')[0].children) {
 				fixElement(child);
@@ -137,19 +181,15 @@ export async function start(): Promise<void> {
 		}
 	}
 
-	reconnect3();*/
+	reconnect3();
 }
 
 export function stop(): void {
 	disabled = true;
 
-	// JS interval method (Old)
-	/*const mediaMosaicExperiment = replugged.webpack.getBySource(/media_mosaic/) as MediaMosaicExperiment;
-	mediaMosaicExperiment.Z.getCurrentConfig().enabled = true;*/
-
-	// JS observer method (Old)
-  /*observer.disconnect();
+	// JS observer method
+  observer.disconnect();
   observer2.disconnect();
   observer3.disconnect();
-	unfixAll();*/
+	unfixAll();
 }
