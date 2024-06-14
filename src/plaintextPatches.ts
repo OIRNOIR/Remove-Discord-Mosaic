6b608b163bcace24600e97abde8e54f004c2c77d
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2024-06-14 10:05:45 -0700

    Lint

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -8,17 +8,18 @@ export default [
 		replacements: [
 			{
 				match: /mediaLayoutType:\w+\.\w+\.MOSAIC/, // 310043
-				replace: 'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
+				replace:
+					'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
 			},
 			{
 				match: /\(0,\w+?\.isImageContentType\)\(\w+?\)\?"IMAGE":\(0,\w+?\.isVideoContentType\)\(\w+?\)\?"VIDEO":"INVALID"/, // 60750
 				replace: '"INVALID"',
 			},
-      {
+			{
 				match: /"IMAGE"===\w+\|\|"VIDEO"===\w+?;/, // 60750
-				replace: 'false;',
+				replace: "false;",
 			},
-      {
+			{
 				match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 69750
 				replace: "true",
 			},
