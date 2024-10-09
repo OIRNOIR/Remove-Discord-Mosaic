7cc8cb3007c96d00ef4045b18f05b8c16563af65
yofukashino <alokender018@gmail.com>
2024-10-09 23:28:11 +0530

    fixes and features

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -3,35 +3,52 @@ import type { PlaintextPatch } from "replugged/types";
 // By yofukashino_
 
 export default [
-	{
-		find: "mp3|m4a|ogg|wav|flac",
+  {
+    find: "mp3|m4a|ogg|opus|wav|flac",
+    replacements: [
+      {
+        match: /\(0,\w+?\.\w+?\)\(\w+?\)\?"IMAGE":\(0,\w+?\.\w+?\)\(\w+?\)\?"VIDEO":"INVALID"/, // 23750
+        replace: '"INVALID"',
+      },
+      {
+        match: /"IMAGE"===\w+\|\|"VIDEO"===\w+?;/, // 23750
+        replace: "false;",
+      },
+    ],
+  },
+  {
+    find: ".gifFavoriteButton,",
+    replacements: [
+      {
+        match: /mediaLayoutType:(\w+\.\w+\.MOSAIC)/, // 884182
+        replace:
+          'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
+      },
+			{
+        match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 23750
+        replace: "true",
+      },
+    ],
+  },
+	{find: "MediaMosaicItem",
 		replacements: [
 			{
-				match: /mediaLayoutType:\w+\.\w+\.MOSAIC/, // 310043
-				replace:
-					'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
-			},
+        match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 546432
+        replace: "true",
+      },
 			{
-				match: /\(0,\w+?\.\w+?\)\(\w+?\)\?"IMAGE":\(0,\w+?\.\w+?\)\(\w+?\)\?"VIDEO":"INVALID"/, // 60750
-				replace: '"INVALID"',
-			},
-			{
-				match: /"IMAGE"===\w+\|\|"VIDEO"===\w+?;/, // 60750
-				replace: "false;",
-			},
-			{
-				match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 69750
-				replace: "true",
-			},
-		],
+        match: /\["VIDEO","CLIP","AUDIO"\]\.includes/, // 546432
+        replace: '["VIDEO","CLIP","AUDIO","IMAGE"].includes',
+      },
+    ],
 	},
-	{
-		find: "Media Mosaic",
-		replacements: [
-			{
-				match: /\w+\.length>1/, // 12067, 938353
-				replace: "false",
-			},
-		],
-	},
+  {
+    find: "Media Mosaic",
+    replacements: [
+      {
+        match: /\w+\.length>1/, // 12067, 938353
+        replace: '(replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("nav", false) ? $& : false)',
+      },
+    ],
+  },
 ] as PlaintextPatch[];
