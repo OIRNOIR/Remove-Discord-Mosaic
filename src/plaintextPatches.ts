fd12b942341f730314f14425a2379a3f460dd1c8
OIRNOIR <70721372+OIRNOIR@users.noreply.github.com>
2024-11-25 15:59:23 -0800

    v1.0.18 - Lint, updates, fixes, new version

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -3,52 +3,53 @@ import type { PlaintextPatch } from "replugged/types";
 // By yofukashino_
 
 export default [
-  {
-    find: "mp3|m4a|ogg|opus|wav|flac",
-    replacements: [
-    /*   {
+	{
+		find: "mp3|m4a|ogg|opus|wav|flac",
+		replacements: [
+			/*   {
         match: /\(0,\w+?\.\w+?\)\(\w+?\)\?"IMAGE":\(0,\w+?\.\w+?\)\(\w+?\)\?"VIDEO":"INVALID"/, // 499376
         replace: '"INVALID"',
       }, */
-      {
-        match: /"IMAGE"===\w+\|\|"VIDEO"===\w+?\|\|"VISUAL_PLACEHOLDER"===\w+?;/, // 499376
-        replace: "false;",
-      },
-    ],
-  },
-  {
-    find: ".gifFavoriteButton,",
-    replacements: [
-      {
-        match: /mediaLayoutType:(\w+\.\w+\.MOSAIC)/, // 884182
-        replace:
-          'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
-      },
+			{
+				match: /"IMAGE"===\w+\|\|"VIDEO"===\w+?\|\|"VISUAL_PLACEHOLDER"===\w+?;/, // 499376
+				replace: "false;",
+			},
+		],
+	},
+	{
+		find: ".gifFavoriteButton,",
+		replacements: [
+			{
+				match: /mediaLayoutType:(\w+\.\w+\.MOSAIC)/, // 884182
+				replace:
+					'mediaLayoutType:replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("static", false) ? "STATIC" : "RESPONSIVE"',
+			},
 			/* {
         match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 23750
         replace: "true",
       }, */
-    ],
-  },
-	{find: "MediaMosaicItem",
+		],
+	},
+	{
+		find: "MediaMosaicItem",
 		replacements: [
 			{
-        match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 546432
-        replace: "true",
-      },
+				match: /[\w_+]===\w+\.\w+\.MOSAIC/, // 546432
+				replace: "true",
+			},
 			{
-        match: /\["VIDEO","CLIP","AUDIO"\]\.includes/, // 546432
-        replace: '["VIDEO","CLIP","AUDIO","IMAGE"].includes',
-      },
-    ],
+				match: /\["VIDEO","CLIP","AUDIO"\]\.includes/, // 546432
+				replace: '["VIDEO","CLIP","AUDIO","IMAGE"].includes',
+			},
+		],
 	},
-  {
-    find: "Media Mosaic",
-    replacements: [
-      {
-        match: /\w+\.length>1/, // 12067, 938353
-        replace: '(replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("nav", false) ? $& : false)',
-      },
-    ],
-  },
+	{
+		find: "Media Mosaic",
+		replacements: [
+			{
+				match: /\w+\.length>1/, // 12067, 938353
+				replace: '(replugged.plugins.getExports("dev.oirnoir.RemoveDiscordMosaic")?.cfg?.get?.("nav", false) ? $& : false)',
+			},
+		],
+	},
 ] as PlaintextPatch[];
